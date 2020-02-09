5370762dd60ec6bfa2041a9da1702b7a8594904a
David Jencks <djencks@apache.org>
2020-02-08 21:31:31 -0800

    fix component name in script

--- docs/old-new-comparison.sh
+++ docs/old-new-comparison.sh
@@ -14,8 +14,8 @@ grep '<loc>' build/site/sitemap-*.xml |sed -E "s/.*(http.*html).*/\1/g" |sort >n
 cat old-sitemap-addr.txt |sed -E "s/(http\:\/\/tomee\.apache\.org.*\/(.*.html))/\2        \1/g"|grep -v "^README.html" | sort >old-sitemap-index-a.txt
 cat old-sitemap-addr.txt |sed -E "s/(http\:\/\/tomee\.apache\.org\/examples\/(.*)\/README.html)/\2.html        \1/g"|grep "   " | sort >old-sitemap-index-b.txt
 sort old-sitemap-index-a.txt old-sitemap-index-b.txt > old-sitemap-index.txt
-#cat new-sitemap-addr.txt |sed -E "s/(https\:\/\/tomee\.apache\.org\/Tomee\/([[:digit:]]\.[[:digit:]])\/(.*.html))/\3      \2        \1/g" |sort >new-sitemap-index.txt
-cat new-sitemap-addr.txt |sed -E "s/(https\:\/\/tomee\.apache\.org\/(Tomee|examples)\/([[:digit:]]\.[[:digit:]]).*\/(.*.html))/\4      \3        \1/g"|sort >new-sitemap-index.txt
+#cat new-sitemap-addr.txt |sed -E "s/(https\:\/\/tomee\.apache\.org\/tomee\/([[:digit:]]\.[[:digit:]])\/(.*.html))/\3      \2        \1/g" |sort >new-sitemap-index.txt
+cat new-sitemap-addr.txt |sed -E "s/(https\:\/\/tomee\.apache\.org\/(tomee|examples)\/([[:digit:]]\.[[:digit:]]).*\/(.*.html))/\4      \3        \1/g"|sort >new-sitemap-index.txt
 
 #Get just the names (first field in "record") for use in grep "not in the other file"
 cat old-sitemap-index.txt |sed -E "s/(^.*.html).*http.*\.html/\1/g"|sort >old-sitemap-names.txt
